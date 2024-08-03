7f911842ec0b89d3aaf9a5c1fefbe8a49ac9a56c
Someperson99 <geryjuarez48147@gmail.com>
2024-08-03 15:46:24 -0700

    added card functionatliy

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 // import reducers
 import topicsReducer from "../features/topics/topicsSlice";
 import quizzesSlice from "../features/quizzes/quizzesSlice";
+import cardsSlice from "../features/cards/cardsSlice";
 
 export default configureStore({
   reducer: {
     topics: topicsReducer,
-    quizzes: quizzesSlice
+    quizzes: quizzesSlice,
+    cards: cardsSlice
   },
 });
