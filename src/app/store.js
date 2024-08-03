d40f5f802b843f404095503467e34d07368501a2
Someperson99 <geryjuarez48147@gmail.com>
2024-08-03 14:28:38 -0700

    recreated topicsSlice.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import reducers
-import { topicsReducer } from '../features/topics/topicsSlice'
 
 export default configureStore({
   reducer: {
-    topics: topicsReducer
   },
 });
